b4359017a599f9cff42c3d76a101b3682f4add06
Lilian Peuron <lilianpeuron@gmail.com>
2024-10-25 18:39:46 +0200

    Refactor scrolling behavior in App component and add Swiper library for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,50 @@ import PageFour from "./components/layouts/PageFour";
 import PageOne from "./components/layouts/PageOne";
 import PageThree from "./components/layouts/PageThree";
 import PageTwo from "./components/layouts/PageTwo";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+  Keyboard,
+} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 function App() {
-  const wrapperRef = useRef(null);
-
-  useEffect(() => {
-    const handleWheel = (e) => {
-      e.preventDefault();
-      if (wrapperRef.current && !isScrolling) {
-        wrapperRef.current.scrollLeft += e.deltaY * 15.2;
-        isScrolling = true;
-        // setTimeout(() => {
-        //   isScrolling = false;
-        // }, 1000);
-      }
-    };
-
-    let isScrolling = false;
-
-    const wrapper = wrapperRef.current;
-    if (wrapper) {
-      wrapper.addEventListener("wheel", handleWheel);
-    }
-
-    return () => {
-      if (wrapper) {
-        wrapper.removeEventListener("wheel", handleWheel);
-      }
-    };
-  }, []);
-
   return (
-    <main
-      ref={wrapperRef}
-      className="flex overflow-x-scroll scroll-smooth max-h-[100vh] overflow-y-hidden"
-    >
-      <PageOne />
-      <PageTwo />
-      <PageThree />
-      <PageFour />
+    <main className="flex overflow-x-scroll scroll-smooth max-h-[100vh] overflow-y-hidden">
+      <Swiper
+        modules={[
+          Navigation,
+          Pagination,
+          Scrollbar,
+          A11y,
+          Mousewheel,
+          Keyboard,
+        ]}
+        spaceBetween={0}
+        slidesPerView={1}
+        pagination={{ type: "fraction", clickable: true }}
+        scrollbar={{ draggable: true }}
+        mousewheel={true}
+        keyboard={{ enabled: true }}
+      >
+        <SwiperSlide>
+          <PageOne />
+        </SwiperSlide>
+        <SwiperSlide>
+          <PageTwo />
+        </SwiperSlide>
+        <SwiperSlide>
+          <PageThree />
+        </SwiperSlide>
+        <SwiperSlide>
+          <PageFour />
+        </SwiperSlide>
+      </Swiper>
     </main>
   );
 }
